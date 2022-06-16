247ddbecd36ab628e2d650bbf64f8204966bdb5b
Hector's Research <hectorsresearch@MacBook-Air.local>
2022-06-16 12:58:19 -0500

    Added beginning build.xml file and edited existing files
    edited useplugin.xml and STMPluginAction.java

--- src/org/tzi/use/STMPlugin/STMPluginAction.java
+++ src/org/tzi/use/STMPlugin/STMPluginAction.java
@@ -1,5 +1,16 @@
 package org.tzi.use.STMPlugin;
 
-public class STMPluginAction {
+import javax.swing.JOptionPane;
+
+import org.tzi.use.gui.main.MainWindow;
+//window for plugin goes here
+import org.tzi.use.runtime.gui.IPluginAction;
+import org.tzi.use.runtime.gui.IPluginActionDelegate;
+
+public class STMPluginAction implements IPluginActionDelegate {
+    @Override
+    public void performAction(IPluginAction pluginAction) {
+        //FILL
+    }
     
 }
