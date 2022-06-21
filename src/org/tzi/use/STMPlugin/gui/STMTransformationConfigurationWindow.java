4e2485c92ef4965fb99675115cd027074e15932f
Hector's Research <hectorsresearch@MacBook-Air.local>
2022-06-21 12:03:28 -0500

    Edited STMTransformationConfigurationWindow
    Added small change to configuration window

--- src/org/tzi/use/STMPlugin/gui/STMTransformationConfigurationWindow.java
+++ src/org/tzi/use/STMPlugin/gui/STMTransformationConfigurationWindow.java
@@ -2,6 +2,8 @@ package org.tzi.use.STMPlugin.gui;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.tzi.use.uml.mm.MModel;
 
@@ -10,6 +12,11 @@ public class STMTransformationConfigurationWindow extends JDialog {
 
     public STMTransformationConfigurationWindow(final JFrame parent, final MModel model) {
         super(parent, "STM Transformation Options");
+
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        //setResizable(false);
+        setVisible(true);
     }
     
     //setDefaultCloseOperation(DISPOSE_ON_CLOSE);
